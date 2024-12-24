29b2cc4beb64c522d02283ad7d53a0660bf04336
fa6om <fa6om@LAPTOP-4OBBIE72>
2024-12-24 14:39:41 +0300

    Add project files.

---
+++ task oop day8/Program.cs
@@ -0,0 +1,10 @@
+﻿using task_oop_day8.modele;
+
+string name, major;
+    int GBA;
+student Student=new student();
+Console.WriteLine("enter your name and major and GBA");
+    name=Console.ReadLine();
+major=Console.ReadLine();
+GBA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Student.getStudent (name ,major,GBA));
---
+++ task oop day8/modele/student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_oop_day8.modele
+{
+    internal class student
+    {
+        public string getStudent(string name, string major, int GBA)
+        {
+            return "name ; " + name + " major ; " + major + " GBA : " + GBA;
+        }
+
+    }
+}
